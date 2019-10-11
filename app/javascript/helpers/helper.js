b4ac1f71682ea6fcb947cdf39293f42c1896ea72
YanFym <dfefeww@gmail.com>
2019-10-11 15:54:12 +0300

    Fix input and algoritm of solving

--- app/javascript/helpers/helper.js
+++ app/javascript/helpers/helper.js
@@ -2,19 +2,21 @@ import { error } from './notifications';
 
 export const isEmptyObject = obj => Object.keys(obj).length === 0;
 
+function isNumber(n) { return /^-?[\d.]+(?:e-?\d+)?$/.test(n); } 
+
 export const validateEquation = (equation) => {
   const errors = {};
 
-  if (equation.senior_cofficient === '') {
-    errors.senior_cofficient = 'You must enter a senior_cofficient';
+  if (equation.senior_cofficient === '' || isNumber(equation.senior_cofficient) == false) {
+    errors.senior_cofficient = 'You must enter a senior_cofficient correctly';
   }
 
-  if (equation.avarage_cofficent === '') {
-    errors.avarage_cofficent = 'You must enter an avarage_cofficent';
+  if (equation.avarage_cofficent === '' || isNumber(equation.avarage_cofficent) == false) {
+    errors.avarage_cofficent = 'You must enter an avarage_cofficent correctly';
   }
 
-  if (equation.free_cofficient === '') {
-    errors.free_cofficient = 'You must enter a free_cofficient';
+  if (equation.free_cofficient === '' || isNumber(equation.free_cofficient) == false) {
+    errors.free_cofficient = 'You must enter a free_cofficient correctly';
   }
 
   return errors;
--- app/models/equation.rb
+++ app/models/equation.rb
@@ -1,8 +1,14 @@
 class Equation < ApplicationRecord
   def fetch_solution
-  	fetch_discriminant
-  	fetch_first_root
-  	fetch_second_root
+    if senior_cofficient == 0
+      simple_equation
+    else
+  	  fetch_discriminant
+      if discriminant >= 0
+        fetch_first_root
+    	  fetch_second_root
+      end
+    end
     Equation.last
   end
 
@@ -24,9 +30,16 @@ class Equation < ApplicationRecord
 
   def fetch_second_root
   	update(second_root:
-  	    (-avarage_cofficent - 
+  	  (-avarage_cofficent - 
   		Math.sqrt(discriminant))/
   		(2 * senior_cofficient))
+  end
+
+  def simple_equation
+    update(first_root:
+      (-free_cofficent)/
+      avarage_cofficent
+      )
   end
 end
 
